6737510aa0ff15d549b15de45b040c72f241e080
tevfik aktas <121631615+tevfikakts@users.noreply.github.com>
2024-03-08 15:44:25 +0300

    Add files via upload

---
+++ Satranc.java
@@ -0,0 +1,23 @@
+package com.mycompany.satranc;
+
+public class Satranc {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    void oyuntahtasi(){
+        char[][] tahta = {
+            {'k', 'a', 'f', 'v', 'ş', 'f', 'a', 'k'},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'K', 'A', 'F', 'V', 'Ş', 'F', 'A', 'K'}
+        };
+        
+    }
+    
+}
